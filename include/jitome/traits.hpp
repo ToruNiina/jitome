14af2355d928ebcd71a75ff41a0c3ad80584cfa3
ToruNiina <niina.toru.68u@gmail.com>
2021-11-19 23:41:45 +0900

    feat: add remove_cvref_t

--- include/jitome/traits.hpp
+++ include/jitome/traits.hpp
@@ -5,6 +5,9 @@
 namespace jitome
 {
 
+template<typename T>
+using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
 template<typename T, typename U>
 constexpr inline bool is_type = std::is_same_v<std::remove_cvref_t<T>, U>;
 
