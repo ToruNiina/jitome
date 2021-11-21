38ead386d309b0c4b35b042b8837ad32ce679137
ToruNiina <niina.toru.68u@gmail.com>
2021-11-21 23:31:15 +0900

    feat: add temporary main file

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "ast.hpp"
+#include "eval.hpp"
+#include <iostream>
+
+int main()
+{
+    jitome::Node root{
+        jitome::NodeExpression<jitome::addition, 2>{
+            {{jitome::make_node_ptr(jitome::NodeImmediate{3.14}),
+              jitome::make_node_ptr(jitome::NodeImmediate{2.71})}}
+        }
+    };
+
+    std::map<std::string, double> env;
+
+    std::cout << jitome::evaluate(env, root) << std::endl;
+}
