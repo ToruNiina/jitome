119f487cd4e483e1df28ddf0ea97632403429c5a
ToruNiina <niina.toru.68u@gmail.com>
2021-11-22 23:39:00 +0900

    fix: fix include path and typo

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include "ast.hpp"
-#include "eval.hpp"
+#include "jitome/ast.hpp"
+#include "jitome/eval.hpp"
 #include <iostream>
 
 int main()
 {
     jitome::Node root{
-        jitome::NodeExpression<jitome::addition, 2>{
+        jitome::NodeExpression<jitome::Addition, 2>{
             {{jitome::make_node_ptr(jitome::NodeImmediate{3.14}),
               jitome::make_node_ptr(jitome::NodeImmediate{2.71})}}
         }
