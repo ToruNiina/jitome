d8112d8d8c6321a2f603c1cf720f104214db203e
ToruNiina <niina.toru.68u@gmail.com>
2021-11-23 19:03:54 +0900

    feat(WIP): add tokenizer implementation

--- include/jitome/tokenizer.hpp
+++ include/jitome/tokenizer.hpp
@@ -2,7 +2,10 @@
 #define JITOME_TOKENIZER_HPP
 #include <memory>
 #include <string>
+#include <string_view>
 #include <vector>
+#include <cassert>
+#include <cctype>
 
 namespace jitome
 {
@@ -24,15 +27,34 @@ struct Token
 {
     TokenKind  kind;
     std::string_view str;
-    std::size_t begin, end; // region index
+    std::size_t begin, len; // region index
     std::shared_ptr<std::string> src;
 };
+
+template<typename Iter>
+Token make_token(TokenKind k, std::shared_ptr<std::string> src, Iter first, Iter last)
+{
+    std::string_view whole(*src);
+    std::size_t begin = std::distance(src->begin(), first);
+    std::size_t len   = std::distance(first, last);
+    return Token{k, whole.substr(begin, len), len, std::move(src)};
+}
 
 template<typename Iter>
 bool is_twochar(Iter iter, Iter end, const char c1, const char c2)
 {
     return (iter != end && *iter == c1 &&
             std::next(iter) != end && *std::next(iter) == c2);
+}
+template<typename Iter, std::size_t N>
+bool is_chars(Iter iter, Iter end, const char (&cs)[N])
+{
+    for(std::size_t i=0; i<N; ++i)
+    {
+        if(iter == end || *iter != cs[i]) {return false;}
+        ++iter;
+    }
+    return true;
 }
 
 template<typename Iter>
@@ -117,22 +139,125 @@ Iter skip_negligible(Iter iter, Iter end)
     return iter;
 }
 
-// template<typename Iter>
-// Result<Token> scan_immediate(Iter& iter, Iter end, std::shared_ptr<std::string> src)
-// {
-//     ;
-// }
-//
-// template<typename Iter>
-// Result<Token> scan_immediate(Iter& iter, Iter end, std::shared_ptr<std::string> src)
-// {
-//     ;
-// }
-// template<typename Iter>
-// Result<Token> scan_immediate(Iter& iter, Iter end, std::shared_ptr<std::string> src)
-// {
-//     ;
-// }
+template<typename Iter>
+Result<Token> scan_immediate(Iter& iter, Iter end, std::shared_ptr<std::string> src)
+{
+    if(iter == end)
+    {
+        return err("scan_immediate: no character left.");
+    }
+    const auto first = iter;
+
+    // unary + and - are tokenized to operator_plus/minus.
+
+    // immediate-real = immediate-real-integer-part
+    //                  [ immediate-real-fractional-part ] [ immeidate-real-exponent-part ]
+
+    // immediate-real-integer-part =  non-zero-digit *digit
+    // immediate-real-integer-part =/ zero
+    if(*iter == '0')
+    {
+        iter = std::next(iter);
+    }
+    else
+    {
+        while(iter != end && std::isdigit(*iter))
+        {
+            iter = std::next(iter);
+        }
+    }
+
+    // immediate-real-fractional-part = %x2E 1*digit
+    if(iter != end && *iter == '.')
+    {
+        iter = std::next(iter);
+
+        if(iter == end || !std::isdigit(*iter))
+        {
+            return err("scan_immediate: at least 1 digit should follow `.`");
+        }
+        while(iter != end && std::isdigit(*iter))
+        {
+            iter = std::next(iter);
+        }
+    }
+
+    // immediate-real-exponent-part   = ( %x65 / %x45 ) 1*digit
+    if(iter != end && (*iter == 'e' || *iter == 'E'))
+    {
+        iter = std::next(iter);
+
+        if(iter == end || !std::isdigit(*iter))
+        {
+            return err("scan_immediate: at least 1 digit should follow `e`");
+        }
+        while(iter != end && std::isdigit(*iter))
+        {
+            iter = std::next(iter);
+        }
+    }
+
+    return make_token(TokenKind::immediate, std::move(src), first, iter);
+}
+
+template<typename Iter>
+Result<Token> scan_identifier(Iter& iter, Iter end, std::shared_ptr<std::string> src)
+{
+    if(iter == end)
+    {
+        return err("scan_immediate: no character left.");
+    }
+    const auto first = iter;
+
+    if(!std::isalpha(*iter))
+    {
+        return err("scan_immediate: the first character of an identifier should be an alphabet.");
+    }
+    iter = std::next(iter);
+
+    while(iter != end && std::isalpha(*iter))
+    {
+        iter = std::next(iter);
+    }
+
+    return make_token(TokenKind::identifier, std::move(src), first, iter);
+}
+
+template<typename Iter>
+Result<Token> scan_operator(Iter& iter, Iter end, std::shared_ptr<std::string> src)
+{
+    if(iter == end)
+    {
+        return err("scan_operator: no character left.");
+    }
+    const auto first = iter;
+
+    if(*iter == '+')
+    {
+        return make_token(TokenKind::operator_plus, std::move(src), first, std::next(iter));
+    }
+    else if(*iter == '-')
+    {
+        return make_token(TokenKind::operator_minus, std::move(src), first, std::next(iter));
+    }
+    else if(*iter == '*')
+    {
+        return make_token(TokenKind::operator_multiply, std::move(src), first, std::next(iter));
+    }
+    else if(*iter == '/')
+    {
+        return make_token(TokenKind::operator_division, std::move(src), first, std::next(iter));
+    }
+    else if(*iter == '(')
+    {
+        return make_token(TokenKind::left_paren, std::move(src), first, std::next(iter));
+    }
+    else if(*iter == ')')
+    {
+        return make_token(TokenKind::right_paren, std::move(src), first, std::next(iter));
+    }
+    return err("scan_operator: unknown operator.");
+}
 
 template<typename Iter>
 Result<Token> scan_token(Iter& iter, Iter end, std::shared_ptr<std::string> src)
