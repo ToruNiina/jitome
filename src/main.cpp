89503795c48185d125b08052c01ac6064f1a62e9
ToruNiina <niina.toru.68u@gmail.com>
2021-11-28 14:42:44 +0900

    feat: change main file into calculator

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
 #include "jitome/ast.hpp"
 #include "jitome/eval.hpp"
+#include "jitome/parser.hpp"
+#include "jitome/tokenizer.hpp"
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-    jitome::Node root{
-        jitome::NodeExpression<jitome::Addition, 2>{
-            {{jitome::make_node_ptr(jitome::NodeImmediate{3.14}),
-              jitome::make_node_ptr(jitome::NodeImmediate{2.71})}}
-        }
-    };
+    if(argc != 2)
+    {
+        std::cerr << "usage: ./jitome 1.0 + 3.14 * 2.71" << std::endl;
+        return 1;
+    }
+
+    const std::string input(argv[1]);
+
+    const auto tokens = jitome::tokenize(input);
+    if(tokens.is_err())
+    {
+        std::cerr << tokens.as_err().msg << std::endl;
+        return 1;
+    }
+
+    const auto root = jitome::parse(tokens.as_val());
+    if(root.is_err())
+    {
+        std::cerr << root.as_err().msg << std::endl;
+        return 1;
+    }
 
     std::map<std::string, double> env;
-
-    std::cout << jitome::evaluate(env, root) << std::endl;
+    std::cout << jitome::evaluate(env, root.as_val()) << std::endl;
+    return 0;
 }
