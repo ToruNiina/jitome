ab775c97ca79d90de823f693ffdfbec7e25cb5ad
ToruNiina <niina.toru.68u@gmail.com>
2021-12-03 00:17:35 +0900

    feat: add jitome::dump(Node)

--- include/jitome/ast.hpp
+++ include/jitome/ast.hpp
@@ -12,6 +12,7 @@ namespace jitome
 struct Node;
 bool operator==(const Node&, const Node&) noexcept;
 bool operator!=(const Node&, const Node&) noexcept;
+std::string dump(const Node&);
 
 struct NodeVariable
 {
@@ -26,6 +27,11 @@ struct NodeVariable
         return this->name != other.name;
     }
 };
+inline std::string dump(const NodeVariable& node)
+{
+    return std::string("Var{") + node.name + "}";
+}
+
 struct NodeImmediate
 {
     double value;
@@ -39,6 +45,10 @@ struct NodeImmediate
         return this->value != other.value;
     }
 };
+inline std::string dump(const NodeImmediate& node)
+{
+    return std::string("Imm{") + std::to_string(node.value) + "}";;
+}
 
 template<typename Func, std::size_t N>
 struct NodeExpression
@@ -81,19 +91,37 @@ struct NodeExpression
     }
 };
 
+template<typename Func, std::size_t N>
+inline std::string dump(const NodeExpression<Func, N>& node)
+{
+    std::string retval("Expr{");
+    retval += Func::dump();
+    retval += "(";
+    bool front = true;
+    for(const auto& expr : node.operands)
+    {
+        if(!front) {retval += ", ";}
+        retval += dump(*expr);
+        front = false;
+    }
+    retval += ")";
+    return retval;
+}
+
 template<typename T> struct is_expr_node : std::false_type {};
 template<typename F, std::size_t N>
 struct is_expr_node<NodeExpression<F, N>> : std::true_type {};
 template<typename T>
 constexpr inline bool is_expr_node_v = is_expr_node<remove_cvref_t<T>>::value;
 
-#define JITOME_DEFINE_BINARY_OPERATION_FUNCTION(name, expr)      \
-    struct name                                                  \
-    {                                                            \
-        static inline double invoke(double x, double y) noexcept \
-        {return (expr);}                                         \
-    };                                                           \
-    using Node ## name = NodeExpression<name, 2>;             /**/
+#define JITOME_DEFINE_BINARY_OPERATION_FUNCTION(name, expr)        \
+    struct name                                                    \
+    {                                                              \
+        static inline double invoke(double x, double y) noexcept   \
+        {return (expr);}                                           \
+        static inline std::string dump() noexcept {return "name";} \
+    };                                                             \
+    using Node ## name = NodeExpression<name, 2>;               /**/
 
 JITOME_DEFINE_BINARY_OPERATION_FUNCTION(Addition,       (x+y))
 JITOME_DEFINE_BINARY_OPERATION_FUNCTION(Subtraction,    (x-y))
@@ -101,13 +129,14 @@ JITOME_DEFINE_BINARY_OPERATION_FUNCTION(Multiplication, (x*y))
 JITOME_DEFINE_BINARY_OPERATION_FUNCTION(Division,       (x/y))
 #undef JITOME_DEFINE_BINARY_OPERATION_FUNCTION
 
-#define JITOME_DEFINE_UNARY_OPERATION_FUNCTION(name, expr) \
-    struct name                                            \
-    {                                                      \
-        static inline double invoke(double x) noexcept     \
-        {return (expr);}                                   \
-    };                                                     \
-    using Node ## name = NodeExpression<name, 1>;       /**/
+#define JITOME_DEFINE_UNARY_OPERATION_FUNCTION(name, expr)         \
+    struct name                                                    \
+    {                                                              \
+        static inline double invoke(double x) noexcept             \
+        {return (expr);}                                           \
+        static inline std::string dump() noexcept {return "name";} \
+    };                                                             \
+    using Node ## name = NodeExpression<name, 1>;               /**/
 
 JITOME_DEFINE_UNARY_OPERATION_FUNCTION(Negation, (-x))
 #undef JITOME_DEFINE_UNARY_OPERATION_FUNCTION
@@ -130,6 +159,24 @@ struct NodeFunction
     }
 };
 
+inline std::string dump(const NodeFunction& node)
+{
+    std::string retval;
+    retval += node.name;
+    retval += "(";
+    bool front = true;
+    for(const auto& arg : node.args)
+    {
+        if(!front) {retval += ", ";}
+        retval += arg;
+        front = false;
+    }
+    retval += ") {";
+    retval += dump(*node.body);
+    retval += "}";
+    return retval;
+}
+
 struct Node
 {
     std::variant<
@@ -142,8 +189,13 @@ struct Node
         NodeNegation,
         NodeFunction
         > node;
+
 };
 
+inline std::string dump(const Node& node)
+{
+    return std::visit([](const auto& n) {return dump(n);}, node.node);
+}
 
 inline bool operator==(const Node& lhs, const Node& rhs) noexcept
 {
