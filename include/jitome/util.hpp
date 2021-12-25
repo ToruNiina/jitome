628b49ac4ae2a5a2e48695bd653b27f62f10939d
ToruNiina <niina.toru.68u@gmail.com>
2021-12-26 00:39:45 +0900

    feat: add copyable_dynamic_storage
    to store and handle dynamically-allocated value in the same way as the
    normal values

--- include/jitome/util.hpp
+++ include/jitome/util.hpp
@@ -1,5 +1,6 @@
 #ifndef JITOME_UTIL_HPP
 #define JITOME_UTIL_HPP
+#include <memory>
 #include <type_traits>
 #include <cstring>
 
@@ -17,6 +18,77 @@ To bit_cast(const From& from)
                 sizeof(From));
     return to;
 }
+
+template<typename T>
+struct copyable_dynamic_storage
+{
+    using value_type = T;
+
+    copyable_dynamic_storage(): storage_(nullptr){}
+
+    template<typename U>
+    copyable_dynamic_storage(U&& v)
+        : storage_(std::make_unique<T>(std::forward<U>(v)))
+    {}
+
+    copyable_dynamic_storage(const copyable_dynamic_storage& other)
+        : storage_(other.storage_ ?
+                   std::make_unique<value_type>(*other.storage_) : nullptr)
+    {}
+
+    copyable_dynamic_storage& operator=(const copyable_dynamic_storage& other)
+    {
+        if(other.storage_)
+        {
+            this->storage_ = std::make_unique<value_type>(*other.storage_);
+        }
+        else
+        {
+            this->storage_.release();
+        }
+    }
+
+    copyable_dynamic_storage(copyable_dynamic_storage&&) = default;
+    copyable_dynamic_storage& operator=(copyable_dynamic_storage&&) = default;
+
+    operator bool() const noexcept {return static_cast<bool>(storage_);}
+
+    operator value_type& () const
+    {
+        return this->get();
+    }
+
+    value_type& get() const
+    {
+        if(!storage_)
+        {
+            throw std::runtime_error("copyable_dynamic_storage: uninitialized");
+        }
+        return *storage_;
+    }
+    value_type& get(std::nothrow_t&) const
+    {
+        return *storage_;
+    }
+
+    bool operator==(const copyable_dynamic_storage& other) const noexcept
+    {
+        if(!(this->storage_) || !(other.storage_))
+        {
+            return this->storage_ == nullptr &&
+                   other.storage_ == nullptr ;
+        }
+        return *(this->storage_) == *(other.storage_);
+    }
+    bool operator!=(const copyable_dynamic_storage& other) const noexcept
+    {
+        return !(*this == other);
+    }
+
+  private:
+
+    std::unique_ptr<T> storage_;
+};
 
 
 } // jitome
